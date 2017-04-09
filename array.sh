b5f9c635cab9c4ae8d17d575e991f927d75086ab
shellever <shellever@163.com>
2017-04-10 00:38:24 +0800

    declare array and use it

---
+++ array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 使用declare命令并指定-a选项来定义一个数组类型变量friends
+declare -a friends
+friends=(shirley pixie zach)
+
+# 获取数组的所有元素
+echo "\${friends[*]}=${friends[*]}"
+# 获取数组的第一个元素
+echo "\${friends[0]}=${friends[0]}"
+# 获取数组的第二个元素
+echo "\${friends[1]}=${friends[1]}"
+# 获取数组的第三个元素
+echo "\${friends[2]}=${friends[2]}"
+# 获取数组的长度
+echo "\${#friends[*]}=${#friends[*]}"
+
