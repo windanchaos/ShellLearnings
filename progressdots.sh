6ddb9ba8f4efc7626c3f7cc8c92ed4f6eec25898
shellever <shellever@163.com>
2017-04-17 23:13:12 +0800

    add comments

--- progressdots.sh
+++ progressdots.sh
@@ -10,9 +10,13 @@ function dots() {
 	done
 }
 
+# 使用&符号使dots运行在后台
 dots 10 &
+# 通过$!获取dots的进程pid
 BG_PID=$!
 
+# 当用户按Ctrl+C来发送中断信号INT时，执行kill命令
+# 防止脚本中断后dots仍在后台运行
 trap "kill -9 $BG_PID" INT
 
 # do the real job here
