912b8395ceec2f00e207271c8101def3be83cfa5
shellever <shellever@163.com>
2017-04-21 21:21:36 +0800

    Linux/Unix Desktop Fun: Terminal Ascii Aquarium

---
+++ asciiquarium/asciiquarium.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Term::Animation
+sudo apt-get install libcurses-perl
+cd ~/Documents/ShellProjects/asciiquarium
+wget http://search.cpan.org/CPAN/authors/id/K/KB/KBAUCOM/Term-Animation-2.4.tar.gz
+tar -zxvf Term-Animation-2.4.tar.gz
+cd Term-Animation-2.4/
+perl Makefile.PL && make && make test
+sudo make install
+
+# Download and Install ASCIIQuarium
+cd ~/Documents/ShellProjects/asciiquarium
+wget http://www.robobunny.com/projects/asciiquarium/asciiquarium.tar.gz
+tar -zxvf asciiquarium.tar.gz
+cd asciiquarium_1.1/
+sudo cp asciiquarium /usr/local/bin
+sudo chmod 0755 /usr/local/bin/asciiquarium
+
+# View ASCII Aquarium
+# /usr/local/bin/asciiquarium
+# or
+# perl /usr/local/bin/asciiquarium
+# or 
+# asciiquarium
