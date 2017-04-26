3fb3486d5c096309ad596c7d798e4ea5b4d4ef05
shellever <shellever@163.com>
2017-04-27 01:49:25 +0800

    change cmd_getopt_basic.sh

--- cmd_getopt_basic.sh
+++ cmd_getopt_basic.sh
@@ -3,14 +3,14 @@
 # ./cmd_getopt_basic.sh type target.zip package.zip
 
 if [ $# -lt 1 ]; then
-		echo "Error, need args."
-		exit 1
+	echo "Error, need args."
+	exit 1
 fi
 
 echo "cmd: $0"
 echo "args: "
 for arg in "$@"
 do
-		echo $arg
+	echo $arg
 done
 
