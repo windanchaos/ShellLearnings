b38451d8fcbb328312045deecc51a6d88bb87ac2
bojiang <bojiang@maike51.com>
2018-01-10 15:54:46 +0800

    判断字符

--- str.sh
+++ str.sh
@@ -3,3 +3,10 @@
 # 字符串长度
 welcome="hello world"
 echo length of \"$welcome\" is ${#welcome}
+
+# 判断字符串为空的方法有三种：
+if [ "$str" =  "" ] 
+if [ x"$str" = x ]
+if [ -z "$str" ] （-n 为非空
+
+#空格，是字符。不属于空的范畴。
