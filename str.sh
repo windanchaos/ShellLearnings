0fe91c4718ab6c780fa85b320b7995dd064f6376
bojiang <bojiang@maike51.com>
2018-02-01 17:35:32 +0800

    sed的使用，及加空格

--- str.sh
+++ str.sh
@@ -10,3 +10,15 @@ if [ x"$str" = x ]
 if [ -z "$str" ] （-n 为非空
 
 #空格，是字符。不属于空的范畴。
+
+#添加空格，需在空格前加\
+#sed 生效需加-i
+#sed 的动作i,需区别
+for rpc in ${WEBENTS_MK[@]} ;do
+        num=`grep -n "</profiles>" ${GIT_HOME}/${rpc}/pom.xml|awk -F ":" '{print $1}'`
+sed -i ${num}'i \        <profile>\
+            <id>nst2</id>\
+            <properties>  \
+                <profiles.active>nst2</profiles.active>  \
+            </properties>\
+        </profile>' ${GIT_HOME}/${rpc}/pom.xml
